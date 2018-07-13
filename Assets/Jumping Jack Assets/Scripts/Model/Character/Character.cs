bf03be3f0d38068557d1729430f1282c754cf57c
Camilo Zuluaga <camilozv18@gmail.com>
2018-07-13 16:40:20 -0500

    Add The first part of hole behaviour

--- Assets/Jumping Jack Assets/Scripts/Model/Character/Character.cs
+++ Assets/Jumping Jack Assets/Scripts/Model/Character/Character.cs
@@ -13,12 +13,13 @@ public class Character : MonoBehaviour {
     private int jumpsAllow;
     private int jumps;
     private Rigidbody2D rb;
+    private Coroutine moveUp;
     #endregion
 
     #region Unity Functions
     void Start()
     {
-        rb = gameObject.AddComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -26,6 +27,20 @@ public class Character : MonoBehaviour {
         if (collision.gameObject.tag == "Floor")
         {
             jumps = 0;
+        }
+
+        if (collision.gameObject.GetComponent<Hole>())
+        {
+            if (moveUp != null)
+            {
+                StopCoroutine(moveUp);
+            }
+            else
+            {
+                moveUp = StartCoroutine(MoveUP(new Vector2(transform.position.x,
+                collision.gameObject.GetComponent<SpriteMask>().bounds.size.y +
+                collision.transform.position.y)));
+            }
         }
     }
     #endregion
@@ -43,6 +58,25 @@ public class Character : MonoBehaviour {
             rb.AddForce(new Vector2(0f, 1f) * jumpForce);
             jumps++;
         }
+    }
+
+    private IEnumerator MoveUP(Vector2 position)
+    {
+        float startTime = Time.time;
+        float lerpTime = 1f;
+        float endTime = startTime + lerpTime;
+        float t;
+        GetComponent<BoxCollider2D>().isTrigger = true;
+        rb.simulated = false;
+        while (Time.time <= endTime)
+        {
+            t = (Time.time - startTime) / lerpTime;
+            transform.position = Vector3.Lerp(transform.position, position, t);
+            yield return new WaitForEndOfFrame();
+        }
+        GetComponent<BoxCollider2D>().isTrigger = false;
+        rb.simulated = true;
+        moveUp = null;
     }
     #endregion
 
