877b43904c46e8e9bdba6f0b91367bdeed9caa1b
Camilo Zuluaga <camilozv18@gmail.com>
2018-07-18 16:59:45 -0500

    Change color of the logo

--- Assets/Jumping Jack Assets/Scripts/Control/Game Control/GameControl.cs
+++ Assets/Jumping Jack Assets/Scripts/Control/Game Control/GameControl.cs
@@ -43,6 +43,7 @@ public class GameControl : MonoBehaviour {
     [SerializeField]
     private AudioSource writingSound;
     #endregion
+
     #region Prefabs
     [SerializeField]
     private GameObject prfCharacter;
